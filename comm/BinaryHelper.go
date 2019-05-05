6dd71f774c9e6a803d1d76993fc4e07fb226dc63
tiptok <785409885@qq.com>
2019-05-05 14:50:47 +0800

    去除uid

--- comm/BinaryHelper.go
+++ comm/BinaryHelper.go
@@ -278,18 +278,18 @@ func (binaryHelper) GetCRC(value []byte) byte {
 func readMachineId() []byte {
 	var sum [3]byte
 	id := sum[:]
-	hostname, err1 := os.Hostname()
-	if err1 != nil {
-		_, err2 := io.ReadFull(rand.Reader, id)
-		if err2 != nil {
-			panic(fmt.Errorf("cannot get hostname: %v; %v", err1, err2))
-		}
-		return id
-	}
-	hw := md5.New()
-	hw.Write([]byte(hostname))
-	copy(id, hw.Sum(nil))
-	fmt.Println("readMachineId:" + string(id))
+	//hostname, err1 := os.Hostname()
+	//if err1 != nil {
+	//	_, err2 := io.ReadFull(rand.Reader, id)
+	//	if err2 != nil {
+	//		panic(fmt.Errorf("cannot get hostname: %v; %v", err1, err2))
+	//	}
+	//	return id
+	//}
+	//hw := md5.New()
+	//hw.Write([]byte(hostname))
+	//copy(id, hw.Sum(nil))
+	//fmt.Println("readMachineId:" + string(id))
 	return id
 }
 
