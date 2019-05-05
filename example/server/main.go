7ab02ac5db54d993a97d7a25951b0ceedd38d89e
tiptok <785409885@qq.com>
2019-05-05 14:57:33 +0800

    demo fix

--- example/server/main.go
+++ example/server/main.go
@@ -35,6 +35,13 @@ func (trans *SimpleServerHandler) OnReceive(c *conn.Connector, d conn.TcpData) b
 }
 
 func main() {
+	defer func(){
+		if p:=recover();p!=nil{
+			log.Println("panic,recover...")
+			log.Println(p)
+		}
+	}()
+
 	var (
 		port int
 	)
